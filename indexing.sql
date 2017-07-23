c5a0e89b0a1265d9f7f9ef59d305e0b065a64442
James Moore <jameslmoore@gmail.com>
2017-07-22 16:18:44 -1000

    added index, time differences, and answered questions in comment at top

--- indexing.sql
+++ indexing.sql
@@ -1,12 +1,32 @@
-DROP USER IF EXISTS 'indexed_cars_user';
+-- 58.280 and 72.159 before indexing and 52.687 and 53.983 after.
+-- last two can't get faster because INT's don't get much faster without sorting the database or partitioning it.
 
-CREATE USER indexed_cars_user;
 
-DROP DATABASE IF EXISTS "indexed_cars";
 
-CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
+-- DROP USER IF EXISTS 'indexed_cars_user';
 
-\timing;
+-- CREATE USER indexed_cars_user;
+
+-- DROP DATABASE IF EXISTS "indexed_cars";
+
+-- CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
+
+-- \timing;
+
+-- SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
+
+-- SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE 'NISSAN' AND model_code LIKE 'GT-R';
+
+-- SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
+
+-- SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+
+-- SELECT * FROM car_models WHERE year = '2010';
+
+
+
+
+CREATE INDEX title_index ON car_models(make_title);
 
 SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
 
