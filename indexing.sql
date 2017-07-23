8fbc586df30f7c4aa096cd51744f5340bebc7679
James Moore <jameslmoore@gmail.com>
2017-07-22 16:40:27 -1000

    time comments

--- indexing.sql
+++ indexing.sql
@@ -1,40 +1,39 @@
--- 58.280 and 72.159 before indexing and 52.687 and 53.983 after.
 -- last two can't get faster because INT's don't get much faster without sorting the database or partitioning it.
 
 
 
--- DROP USER IF EXISTS 'indexed_cars_user';
+DROP USER IF EXISTS 'indexed_cars_user';
 
--- CREATE USER indexed_cars_user;
+CREATE USER indexed_cars_user;
 
--- DROP DATABASE IF EXISTS "indexed_cars";
+DROP DATABASE IF EXISTS "indexed_cars";
 
--- CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
+CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
 
--- \timing;
+\timing;
 
--- SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
+SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';--100.185
 
--- SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE 'NISSAN' AND model_code LIKE 'GT-R';
+SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE 'NISSAN' AND model_code LIKE 'GT-R';--61.665
 
--- SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';--81.965
 
--- SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;--409.522
 
--- SELECT * FROM car_models WHERE year = '2010';
-
-
+SELECT * FROM car_models WHERE year = '2010';--103.308
 
 
-CREATE INDEX title_index ON car_models(make_title);
--- CREATE INDEX lam_index ON car_models(make_title) WHERE make_code LIKE '%LAM%';
 
-SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
 
-SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE 'NISSAN' AND model_code LIKE 'GT-R';
+-- CREATE INDEX title_index ON car_models(make_title);
+CREATE INDEX lam_index ON car_models(make_title) WHERE make_code LIKE '%LAM%';
 
-SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';
+SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';--3.328
 
-SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE 'NISSAN' AND model_code LIKE 'GT-R';--65.563
 
-SELECT * FROM car_models WHERE year = '2010';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code LIKE '%LAM%';--1.851
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;--183.792
+
+SELECT * FROM car_models WHERE year = '2010';--84.424;
