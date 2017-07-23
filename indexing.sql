3bcb297b431b68124927acbe3436299a9627eecc
James Moore <jameslmoore@gmail.com>
2017-07-22 15:59:08 -1000

    sql queries

--- indexing.sql
+++ indexing.sql
@@ -6,3 +6,8 @@ DROP DATABASE IF EXISTS "indexed_cars";
 
 CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
 
+\timing;
+
+SELECT DISTINCT make_title FROM car_models WHERE make_code LIKE '%LAM%';
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code LIKE "NISSAN" AND model_code LIKE "GT-R";
