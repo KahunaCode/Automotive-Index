6b39aa23c8b21212f1e17eb44349290b5dbf31f8
James Moore <jameslmoore@gmail.com>
2017-07-22 15:41:02 -1000

    basic db setup

---
+++ indexing.sql
@@ -0,0 +1,8 @@
+DROP USER IF EXISTS 'indexed_cars_user';
+
+CREATE USER indexed_cars_user;
+
+DROP DATABASE IF EXISTS "indexed_cars";
+
+CREATE DATABASE "indexed_cars" WITH OWNER indexed_cars_user;
+
